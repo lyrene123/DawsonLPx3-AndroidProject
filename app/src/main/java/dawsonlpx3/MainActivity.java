e83423a4ef1a9c8e070e40325c7d8ce9d1a248fc
Lyrene <laborlyrene@gmail.com>
2017-11-20 14:40:15 -0500

    Implemented code for startup check for shared prefs to retrieve user input from register page. Method created is checkForUserAuthentication in the MainActivity and will check if user credentials exist in the shared pref and if not, then launch the register activity

--- app/src/main/java/dawsonlpx3/MainActivity.java
+++ app/src/main/java/dawsonlpx3/MainActivity.java
@@ -1,13 +1,45 @@
 package dawsonlpx3;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "LPx3-Main";
+    private String FNAME, LNAME, PASSWORD, EMAIL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate launched");
+        checkForUserAuthentication();
+
+
+
+    }
+
+    private void checkForUserAuthentication(){
+        Log.d(TAG, "checkForUserAuthentication launched");
+
+        //retrieve from shared prefs any existing user credentials.
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        this.FNAME = prefs.getString("fname", null);
+        this.LNAME = prefs.getString("lname", null);
+        this.PASSWORD = prefs.getString("password", null);
+        this.EMAIL = prefs.getString("email", null);
+
+        //if no or some credentials missing, then launch the register activity
+        if(this.FNAME == null || this.LNAME == null || this.PASSWORD == null ||
+                this.EMAIL == null){
+            Log.d(TAG,"Launching register activity");
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        }
     }
 }

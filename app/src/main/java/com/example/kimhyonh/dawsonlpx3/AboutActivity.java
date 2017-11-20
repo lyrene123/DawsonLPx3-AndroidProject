169e70e1cad3caf64bd7e363972b62f5372f83e6
P Bellefleur <bellefleurpeter@yahoo.ca>
2017-11-20 14:36:51 -0500

    Create base code for AboutActivity and its layout

--- app/src/main/java/com/example/kimhyonh/dawsonlpx3/AboutActivity.java
+++ app/src/main/java/com/example/kimhyonh/dawsonlpx3/AboutActivity.java
@@ -1,9 +1,17 @@
 package com.example.kimhyonh.dawsonlpx3;
 
+import android.os.Bundle;
+
 /**
  * TODO: javadocs, implementation
  */
 
-public class AboutActivity {
+public class AboutActivity extends MenuActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
 
 }

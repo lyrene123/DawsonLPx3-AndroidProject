238d1f9765287f620d570bd2fdb1389a2efa50f6
P Bellefleur <bellefleurpeter@yahoo.ca>
2017-11-20 14:40:08 -0500

    Create base code for SettingsActivity

--- app/src/main/java/com/example/kimhyonh/dawsonlpx3/SettingsActivity.java
+++ app/src/main/java/com/example/kimhyonh/dawsonlpx3/SettingsActivity.java
@@ -1,9 +1,17 @@
 package com.example.kimhyonh.dawsonlpx3;
 
+import android.os.Bundle;
+
 /**
  * TODO: javadocs, implementation
  */
 
-public class SettingsActivity {
+public class SettingsActivity extends MenuActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
 
 }

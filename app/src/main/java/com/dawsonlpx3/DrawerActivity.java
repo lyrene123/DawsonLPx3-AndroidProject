dabc615d43fc0f6cc01e0f7938d99bd091cb9115
kimhyonh <pengkim.professional@gmail.com>
2017-11-20 21:05:33 -0500

    Implementing drawer! So all the activities need to be fragment.

---
+++ app/src/main/java/com/dawsonlpx3/DrawerActivity.java
@@ -0,0 +1,121 @@
+package com.dawsonlpx3;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class DrawerActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        FragmentManager fragmentManager = getFragmentManager();
+
+        if (id == R.id.nav_classCancel) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, new CanceledActivity())
+                    .commit();
+        } else if (id == R.id.nav_findTeacher) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, new FindTeacherActivity())
+                    .commit();
+        } else if (id == R.id.nav_addToCalendar) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, new CalendarActivity())
+                    .commit();
+        } else if (id == R.id.nav_note) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, new NotesActivity())
+                    .commit();
+        } else if (id == R.id.nav_acedemicCalendar) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, new AcedemicCalendarActivity())
+                    .commit();
+        } else if (id == R.id.nav_weather) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, new WeatherActivity())
+                    .commit();
+        } else if (id == R.id.nav_about) {
+
+        } else if (id == R.id.nav_setting) {
+
+        } else if (id == R.id.nav_dawson) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}

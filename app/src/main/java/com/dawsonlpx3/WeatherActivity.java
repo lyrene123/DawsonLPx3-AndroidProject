4875f71f58945de538a48ba2d08fb2d4c4f18a4e
Philippe Langlois <planglois.pedroso@gmail.com>
2017-11-21 11:41:27 -0500

    Revert "Implementing drawer! So all the activities need to be fragment."

--- app/src/main/java/com/dawsonlpx3/WeatherActivity.java
+++ app/src/main/java/com/dawsonlpx3/WeatherActivity.java
@@ -1,21 +1,13 @@
 package com.dawsonlpx3;
 
-import android.app.Fragment;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-public class WeatherActivity extends Fragment {
+public class WeatherActivity extends AppCompatActivity {
 
-    View view;
-
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.activity_weather, container, false);
-        return view;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
     }
 }

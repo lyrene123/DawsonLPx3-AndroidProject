4d0932c46c386925d7461d84e2a0d2f0ba81fba2
Lyrene <laborlyrene@gmail.com>
2017-11-23 21:23:02 -0500

    Started implementing the FindTeacher feature:
    - In the FindTeacherActivity, implemented the onViewCreated in order to retrieve the necessary widgets from the view.
    - In the layout xml, implemented the RadioGroup in order to group the radio buttons of like/exact search together.
    - Added a TextView in the layout that will contain error messages.
    - Added validation code in the findteacheractivity

--- app/src/main/java/com/dawsonlpx3/FindTeacherActivity.java
+++ app/src/main/java/com/dawsonlpx3/FindTeacherActivity.java
@@ -4,23 +4,74 @@ import android.app.Fragment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class FindTeacherActivity extends Fragment {
+public class FindTeacherActivity extends Fragment implements View.OnClickListener {
 
-    View view;
+    private View view; //view containing the fragment
+    private EditText fnameET, lnameET;
+    private RadioButton likeRB, exactRB;
+    private TextView errorMsgTV;
+    private Button searchButton;
+
+    private final String TAG = "DawsonLPx3-FindTeacher";
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView started");
         view = inflater.inflate(R.layout.activity_find_teacher, container, false);
         return view;
     }
 
-    //    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_find_teacher);
-//    }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        Log.d(TAG, "onViewCreated started");
+        fnameET = (EditText) view.findViewById(R.id.firstnameEditText);
+        lnameET = (EditText) view.findViewById(R.id.lastnameEditText);
+        likeRB = (RadioButton) view.findViewById(R.id.likeRadioBtn);
+        exactRB = (RadioButton) view.findViewById(R.id.exactRadioBtn);
+        errorMsgTV = (TextView) view.findViewById(R.id.errorMsgTV);
+        searchButton = (Button) view.findViewById(R.id.search_button);
+        searchButton.setOnClickListener(this);
+    }
+
+    public void searchTeacher(View view) {
+        if(isValidInput()){
+            boolean isExact = exactRB.isChecked();
+            String fullname = (fnameET.getText().toString() + " " + lnameET.getText().toString()).trim();
+            Log.d(TAG, "isEaxt: " + isExact + "  fullname: " + fullname);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.search_button:
+                Log.d(TAG, "search button clicked started");
+                searchTeacher(v);
+                break;
+        }
+    }
+
+    private boolean isValidInput(){
+        if((fnameET.getText() == null || fnameET.getText().toString().isEmpty())
+                && (lnameET.getText() == null || lnameET.getText().toString().isEmpty())){
+            errorMsgTV.setText(R.string.invalidTeacherSearch);
+            return false;
+        }
+        return true;
+    }
+
+
 }
--- app/src/main/java/com/dawsonlpx3/MainActivity.java
+++ app/src/main/java/com/dawsonlpx3/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.support.design.widget.NavigationView;
+import android.view.View;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;

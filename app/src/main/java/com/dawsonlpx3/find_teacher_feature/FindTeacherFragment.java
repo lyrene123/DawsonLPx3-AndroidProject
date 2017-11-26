aa39e9f34d46f0e013282189a2a2e81711f427a2
Lyrene <laborlyrene@gmail.com>
2017-11-26 00:27:29 -0500

    Added javadocs for the FindTeacherFragment class and TeacherContactFragment class and all of their methods + organized code as well

--- app/src/main/java/com/dawsonlpx3/find_teacher_feature/FindTeacherFragment.java
+++ app/src/main/java/com/dawsonlpx3/find_teacher_feature/FindTeacherFragment.java
@@ -21,9 +21,24 @@ import com.dawsonlpx3.db.FirebaseManagerUtil;
 
 import java.util.List;
 
+/**
+ * Displays the search form for the user to enter a first name and last name to search
+ * teachers. Handles the validation of the user input. Handles the call to the
+ * firebase manager util instance to query the database to retrieve the records of teachers
+ * that matches the search criteria of the user. Manages the display of the display of the
+ * search result which can be a list of teachers or a teacher by inflating the appropriate
+ * fragment (ChooseTeacherFragment or TeacherContactFragment).
+ *
+ * @author Lyrene Labor
+ * @author Pengkim Sy
+ * @author Phil Langlois
+ * @author Peter Bellefleur
+ */
 public class FindTeacherFragment extends Fragment implements View.OnClickListener {
 
     private View view; //view containing the fragment
+
+    //widgets taken from the view
     private EditText fnameET, lnameET;
     private RadioButton likeRB, exactRB;
     private TextView errorMsgTV;
@@ -39,7 +54,14 @@ public class FindTeacherFragment extends Fragment implements View.OnClickListene
 
     private final String TAG = "DawsonLPx3-FindTeacher";
 
-
+    /**
+     * Initializes the FirebaseManagerUtil object to be used when retrieving records from the
+     * firebase database. If there are existing items in the Bundle, restore those items which are
+     * the input strings inside the EditText of the view's search form. If none existing in the Bundle,
+     * then initialize the properties to empty strings.
+     *
+     * @param savedInstanceState Bundle object
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,6 +79,14 @@ public class FindTeacherFragment extends Fragment implements View.OnClickListene
         }
     }
 
+    /**
+     * Inflates the layout into the fragment and returns the view containing the fragment.
+     *
+     * @param inflater LayoutInflater object
+     * @param container ViewGroup object
+     * @param savedInstanceState Bundle object
+     * @return View containing the fragment
+     */
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -65,6 +95,9 @@ public class FindTeacherFragment extends Fragment implements View.OnClickListene
         return view;
     }
 
+    /**
+     * Sets the isOnSavedInstanceState boolean back to false once the app is on state Resumed
+     */
     @Override
     public void onResume() {
         super.onResume();
@@ -72,6 +105,14 @@ public class FindTeacherFragment extends Fragment implements View.OnClickListene
         this.isOnSavedInstanceState = false;
     }
 
+    /**
+     * Retrieves the necessary widgets from the view and sets the restored values
+     * in the first name, last name EditText if there are any restored values from the
+     * Bundle. If none, then the EditTexts will be empty.
+     *
+     * @param view View of fragment
+     * @param savedInstanceState Bundle object
+     */
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         Log.d(TAG, "onViewCreated started");
@@ -88,11 +129,20 @@ public class FindTeacherFragment extends Fragment implements View.OnClickListene
         errorMsgTV.setText(this.restoredErrorMsg);
     }
 
-    public void searchTeacher(View view) {
+    /**
+     * Searches for records of teachers matching the search criteria of the user through the
+     * FirebaseManager util class. Validates the input of the user first and if valid,
+     * sets the isSearched boolean to true, empties the error message TextView, and retrieves
+     * the input of the user (first name and/or last name). Starts a seperate async task for
+     * querying the firebase database.
+     */
+    public void searchTeacher() {
         if(isValidInput()){
             isSearched = true;
             errorMsgTV.setText("");
-            isExact = exactRB.isChecked();
+            isExact = exactRB.isChecked(); //retrieve user's choice of exact/like search
+
+            //retrieve the input of the user for first and/or last name.
             if(!fnameET.getText().toString().isEmpty() && !lnameET.getText().toString().isEmpty()){
                 fullname = fnameET.getText().toString().trim() + " " + lnameET.getText().toString().trim();
                 Log.d(TAG, "isExact: " + isExact + "  fullname: " + fullname);
@@ -103,20 +153,32 @@ public class FindTeacherFragment extends Fragment implements View.OnClickListene
                 lname = lnameET.getText().toString().trim();
                 Log.d(TAG, "isExact: " + isExact + " lname: " + lname);
             }
-            //this.fbManager.retrieveRecordsFromDb(getActivity(), this, fullname, fname, lname, isExact);
+
+            //start seperate async task for querying the db
             this.teachersTask = new GetTeachersTask();
             this.teachersTask.execute();
             Toast.makeText(getActivity(), getResources().getString(R.string.searchTeachers), Toast.LENGTH_SHORT).show();
         }
     }
 
+    /**
+     * Sets the teachers list containing the result of the user's search. List contains TeacherDetails
+     * objects which will then be used to display as a list in ChooseTeacherFragment if there are
+     * more than one teacher in the list. If only one teacher in the result list, then all details
+     * of that one teacher will be displayed in TeacherContactFragment.
+     *
+     * This method is called by the FirebaseManagerUtil after all records have been restored
+     *
+     * @param teachersList List of TeacherDetails objects
+     */
     public void setTeachersList(List<TeacherDetails> teachersList){
         teachers = teachersList;
         isSearched = false;
         Log.d(TAG, "num of teacher records: " + teachers.size());
         Log.d(TAG, "task: " + teachersTask + " isSaved...: " + isOnSavedInstanceState);
+
+        //inflate the fragments only if the async task is not cancelled and OnSavedInstanceState is not called
         if(teachersTask != null && !this.isOnSavedInstanceState) {
-            //display ChooseTeacher fragment
             if (teachers.size() > 1) {
                 displayChooseTeacher();
             } else if (teachers.size() == 1) {
@@ -127,13 +189,21 @@ public class FindTeacherFragment extends Fragment implements View.OnClickListene
         }
     }
 
+    /**
+     * Inflates the TeacherDetailFragment with a single TeacherDetails object in order to display
+     * all information of a teacher. Passes the TeacherDetails object to display in a Bundle and
+     * that bundle is passed as arguments to the TeacherDetailFragment fragment.
+     */
     private void displayTeacherDetail(){
         Log.d(TAG, "displayTeacherDetail started");
         TeacherContactFragment teacherContactFragment = new TeacherContactFragment();
+
+        //pass the TeacherDetails object to display as an argument to the fragment
         Bundle args = new Bundle();
         args.putSerializable("teacher", teachers.get(0));
         teacherContactFragment.setArguments(args);
 
+        //inflate the fragment
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.content_frame, teacherContactFragment)
@@ -141,30 +211,47 @@ public class FindTeacherFragment extends Fragment implements View.OnClickListene
                 .commit();
     }
 
+    /**
+     * Inflates the ChooseTeacherFragment which will display the list of teachers resulting
+     * from the search to the database. Sets the list of teachers to display through the
+     * setTeachersList method of the ChooseTeacherFragment class.
+     */
     private void displayChooseTeacher(){
         Log.d(TAG, "displayChooseTeacher started");
         ChooseTeacherFragment chooseTeacherFragment = new ChooseTeacherFragment();
-        chooseTeacherFragment.setTeachersList(teachers);
-        FragmentManager fragmentManager = getFragmentManager();
+        chooseTeacherFragment.setTeachersList(teachers); //pass the list to display
+        FragmentManager fragmentManager = getFragmentManager(); //inflate fragment
         fragmentManager.beginTransaction()
                 .replace(R.id.content_frame, chooseTeacherFragment)
                 .addToBackStack(null)
                 .commit();
-
     }
 
+    /**
+     * Handles the onclick of the current fragment. Verifies the id of the view that was
+     * clicked and calls the appropriate method handler.
+     *
+     * @param v View that was clicked
+     */
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.search_button:
                 Log.d(TAG, "search button clicked started");
+                //if search button was clicked and not currently already querying the database
                 if(!isSearched) {
-                    searchTeacher(v);
+                    searchTeacher();
                 }
                 break;
         }
     }
 
+    /**
+     * Verifies if the user input has at least entered a first name or a last name
+     * in the input of the search form
+     *
+     * @return boolean if user input is valid
+     */
     private boolean isValidInput(){
         if((fnameET.getText() == null || fnameET.getText().toString().isEmpty())
                 && (lnameET.getText() == null || lnameET.getText().toString().isEmpty())){
@@ -175,6 +262,11 @@ public class FindTeacherFragment extends Fragment implements View.OnClickListene
         return true;
     }
 
+    /**
+     * Saves the user input to the Bundle state if there are any input.
+     *
+     * @param outState Bundle object
+     */
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -191,6 +283,10 @@ public class FindTeacherFragment extends Fragment implements View.OnClickListene
         }
     }
 
+    /**
+     * If app is on DestroyView state, then stop the async task for searching Teacher and
+     * set the task to null only if task is not null which means that there is an existing task.
+     */
     @Override
     public void onDestroyView() {
         super.onDestroyView();
@@ -201,8 +297,20 @@ public class FindTeacherFragment extends Fragment implements View.OnClickListene
         }
     }
 
+    /**
+     * Task class extending AsyncTask used to query the firebase database for teacher records
+     * matching the search criteria of the user.
+     */
     private class GetTeachersTask extends AsyncTask<Void, Void, List<TeacherDetails>> {
 
+        /**
+         * Retrieve records from the firebase database matching the search criteria of the user
+         * in an async task running in the background.
+         *
+         * @param voids
+         * @return null in this case since the setTeachersList method will be invoked by the firebase
+         *          util instance once all records have been retrieved from database.
+         */
         @Override
         protected List<TeacherDetails> doInBackground(Void... voids) {
             Log.d(TAG, "Starting GetTeachersTask async task");
--- app/src/main/java/com/dawsonlpx3/find_teacher_feature/TeacherContactFragment.java
+++ app/src/main/java/com/dawsonlpx3/find_teacher_feature/TeacherContactFragment.java
@@ -23,15 +23,36 @@ import com.dawsonlpx3.data.TeacherDetails;
 
 import java.util.List;
 
+/**
+ * Fragment class which handles the display of all contact details of a Dawson college teacher.
+ * Handles the click events of the local and the email address of the teacher which starts a phone call
+ * to contact the teacher or creates an email to send to the teacher's email address. Handles as
+ * well the saving and restoring of the teacher object when app is being stoped or paused. Requires
+ * the necessary permissions to the user when trying to access the phone of their device.
+ *
+ * @author Lyrene Labor
+ * @author Pengkim Sy
+ * @author Phil Langlois
+ * @author Peter Bellefleur
+ */
 public class TeacherContactFragment extends Fragment {
-    private TeacherDetails teacher;
+    private TeacherDetails teacher; //TeacherDetails object to display
+
+    //widgets taken from the view
     private TextView fnameTV, lnameTV, emailTV, localTV, positionTV, departmentTV, sectorTV, officeTV,
             websiteTV, bioTV;
     private ImageView imageView;
     private View mainView;
+
     private final String TAG = "Dawson-TeacherContact";
     private static final int PERMISSION_REQUEST_CALL = 0;
 
+    /**
+     * Retrieves the teacher object to be displayed from the arguments passed to the fragment if there is. If not, then
+     * retrieve the teacher object from the Bundle
+     *
+     * @param savedInstanceState Bundle object
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate started");
@@ -46,11 +67,26 @@ public class TeacherContactFragment extends Fragment {
         }
     }
 
+    /**
+     * Inflates the layout into the fragment and returns the view containing the fragment
+     *
+     * @param inflater LayoutInflater object
+     * @param parent ViewGroup object
+     * @param savedInstanceState Bundle object
+     * @return View containing the fragment
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.activity_teacher_contact, parent, false);
     }
 
+    /**
+     * Retrieves the necessary widgets from the view and then display the teacher details into those
+     * widgets
+     *
+     * @param view View of the fragment
+     * @param savedInstanceState Bundle object
+     */
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         this.fnameTV = (TextView) view.findViewById(R.id.firstnameTextView);
@@ -66,10 +102,12 @@ public class TeacherContactFragment extends Fragment {
         this.imageView = (ImageView) view.findViewById(R.id.imageImageView);
         this.mainView = view.findViewById(R.id.teacherContactLayout);
         displayTeacherDetails();
-
-
     }
 
+    /**
+     * Creates an intent for sending an email and starts the intent and display an email to be sent
+     * to the teacher in display
+     */
     private void sendEmail(){
         Log.d(TAG, "Sending email....");
         Intent intent = new Intent(Intent.ACTION_SENDTO);
@@ -80,29 +118,43 @@ public class TeacherContactFragment extends Fragment {
         startActivity(intent);
     }
 
+    /**
+     * Displays the teacher's information to the appropriate widget in the view. Verifies first if the
+     * detail to display is not null nor empty in order to display the detail. If detail is null, then
+     * the default string in the widgets is 'unavailable'. If this check if not done, then some
+     * text views will be completely empty.
+     */
     private void displayTeacherDetails(){
         Log.d(TAG, "displayTeacherDetails started");
-
-        if(this.teacher.getFirst_name() == null || this.teacher.getFirst_name().isEmpty()){
-            this.fnameTV.setText(getResources().getString(R.string.unavailable));
-        }else{
+        //display first name
+        if(this.teacher.getFirst_name() != null && !this.teacher.getFirst_name().isEmpty()) {
             this.fnameTV.setText(this.teacher.getFirst_name());
         }
-
-        if(this.teacher.getLast_name() == null || this.teacher.getLast_name().isEmpty()){
-            this.lnameTV.setText(getResources().getString(R.string.unavailable));
-        }else {
+        //display last name
+        if(this.teacher.getLast_name() != null && !this.teacher.getLast_name().isEmpty()) {
             this.lnameTV.setText(this.teacher.getLast_name());
         }
+        //display teacher office
+        if(this.teacher.getOffice() != null && !this.teacher.getOffice().isEmpty()) {
+            this.officeTV.setText(this.teacher.getOffice());
+        }
+        //display website
+        if(this.teacher.getWebsite() != null && !this.teacher.getWebsite().isEmpty()) {
+            this.websiteTV.setText(this.teacher.getWebsite());
+        }
+        //display bio
+        if(this.teacher.getBio() != null && !this.teacher.getBio().isEmpty()) {
+            this.bioTV.setText(this.teacher.getBio());
+        }
 
-        if(this.teacher.getEmail() == null || this.teacher.getEmail().isEmpty()){
-            this.emailTV.setText(getResources().getString(R.string.unavailable));
-        }else {
-            //the following is to underline the attributed name
+        this.imageView.setImageResource(R.mipmap.ic_launcher); //default image since teacher image not provided
+
+        //the following is to underline the email and make it clickable only if email is not null nor empty
+        if(this.teacher.getEmail() != null && !this.teacher.getEmail().isEmpty()){
             SpannableString content = new SpannableString(Html.fromHtml(this.teacher.getEmail()).toString());
             content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
             this.emailTV.setText(content);
-
+            //only make the email clickable if there is an existing email
             emailTV.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -111,32 +163,13 @@ public class TeacherContactFragment extends Fragment {
             });
         }
 
-        if(this.teacher.getOffice() == null || this.teacher.getOffice().isEmpty()){
-            this.officeTV.setText(getResources().getString(R.string.unavailable));
-        } else {
-            this.officeTV.setText(this.teacher.getOffice());
-        }
-
-        if(this.teacher.getWebsite() == null || this.teacher.getWebsite().isEmpty()){
-            this.websiteTV.setText(getResources().getString(R.string.unavailable));
-        } else {
-            this.websiteTV.setText(this.teacher.getWebsite());
-        }
-
-        if(this.teacher.getBio() == null || this.teacher.getBio().isEmpty()){
-            this.bioTV.setText(getResources().getString(R.string.unavailable));
-        } else {
-            this.bioTV.setText(this.teacher.getBio());
-        }
-
-        if(this.teacher.getLocal() == null || this.teacher.getLocal().isEmpty()){
-            this.localTV.setText(getResources().getString(R.string.unavailable));
-        }else {
+        //the following is to underline the local and make it clickable only if local is not null nor empty
+        if(this.teacher.getLocal() != null && !this.teacher.getLocal().isEmpty()){
             //the following is to underline the attributed name
             SpannableString content = new SpannableString(this.teacher.getLocal());
             content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
             this.localTV.setText(content);
-
+            //only make the local clickable if there is an existing local
             localTV.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -145,48 +178,52 @@ public class TeacherContactFragment extends Fragment {
             });
         }
 
-        this.imageView.setImageResource(R.mipmap.ic_launcher);
-
+        //display the positions
         List<Object> positions = this.teacher.getPositions();
         if(positions != null && positions.size() > 0) {
+            this.positionTV.setText("");
             for (int i = 0; i < positions.size(); i++) {
                 this.positionTV.setText(this.positionTV.getText() + "\n" + positions.get(i));
             }
-        } else {
-            this.positionTV.setText(getResources().getString(R.string.unavailable));
         }
-
+        //display the departments
         List<Object> departments = this.teacher.getDepartments();
         if(departments != null && departments.size() > 0) {
+            this.departmentTV.setText("");
             for (int i = 0; i < departments.size(); i++) {
                 this.departmentTV.setText(this.departmentTV.getText() + "\n" + departments.get(i));
             }
-        }else {
-            this.departmentTV.setText(getResources().getString(R.string.unavailable));
         }
-
+        //display the sectors
         List<Object> sectors = this.teacher.getSectors();
         if(sectors != null && sectors.size() > 0) {
+            this.sectorTV.setText("");
             for (int i = 0; i < sectors.size(); i++) {
                 this.sectorTV.setText(this.sectorTV.getText() + "\n" + sectors.get(i));
             }
-        } else {
-            this.sectorTV.setText(getResources().getString(R.string.unavailable));
         }
-
-
     }
 
+    /**
+     * Saves the teacher object that is being displayed so that even after device rotation,
+     * the data being displayed will be restored back to the view.
+     *
+     * @param outState Bundle object
+     */
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putSerializable("teacher", this.teacher);
     }
 
-    //https://github.com/googlesamples/android-RuntimePermissionsBasic
+    /**
+     * Starts a phone call by checking if app has already the right permissions to open the phone
+     * of the device. If not, then the permission will be requested to the user.
+     *
+     * Solution found: https://github.com/googlesamples/android-RuntimePermissionsBasic
+     */
     private void dialPhoneNumber() {
         Log.d(TAG, "Calling a teacher....");
-
         if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.CALL_PHONE)
                 == PackageManager.PERMISSION_GRANTED) {
             startCall();
@@ -195,7 +232,13 @@ public class TeacherContactFragment extends Fragment {
         }
     }
 
+    /**
+     * Requests permission to the user to open phone of their device and process the answer of the
+     * user. A message will be displayed to the user about why the permission is necessary if applicable.
+     * If a message is not needed, then proceeds to acquire the permission.
+     */
     private void requestCallPermission() {
+        Log.d(TAG, "Requesting for phone call permission");
         if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
                 android.Manifest.permission.CALL_PHONE)) {
             Snackbar.make(this.mainView, getResources().getString(R.string.phone_access_required),
@@ -217,6 +260,16 @@ public class TeacherContactFragment extends Fragment {
                     PERMISSION_REQUEST_CALL);
         }
     }
+
+    /**
+     * Process the user's answer of whether or not to grant permission to let the app access the phone
+     * of their device. If permission granted, then the phone call will be started. If not,
+     * then a message will be displayed letting the user know that the phone call is denied.
+     *
+     * @param requestCode int request code
+     * @param permissions string array of permissions requested
+     * @param grantResults int array of the result code
+     */
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions,
                                            int[] grantResults) {
@@ -238,16 +291,22 @@ public class TeacherContactFragment extends Fragment {
         }
     }
 
-    //https://stackoverflow.com/questions/18216563/making-a-phone-call-with-a-number-extension
+    /**
+     * Starts a phone call by creating and launching an intent and passing it the phone call and the extension
+     * of the teacher to contact.
+     */
     private void startCall(){
         Log.d(TAG, "startCall");
         Intent intent = new Intent(Intent.ACTION_CALL);
         String dawsonNumber;
         String ext = "";
+
+        //if retrieved local is only an extension number, append the dawson number
         if(teacher.getLocal().length() <= 4) {
             dawsonNumber  = getResources().getString(R.string.dawsonPhoneNum);
             ext = ","+teacher.getLocal();
         } else {
+            //if retrieved local is a complete phone number, dont append the dawson number
             dawsonNumber = teacher.getLocal();
         }
 

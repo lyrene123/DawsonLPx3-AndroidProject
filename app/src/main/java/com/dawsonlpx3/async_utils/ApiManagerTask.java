cafe7dfbb19ddb660621bc248a4ad81c748ce03b
Lyrene <laborlyrene@gmail.com>
2017-12-01 17:02:08 -0500

    Added seperate async class for each api call made to the backend as requested by Phil in the PR

--- app/src/main/java/com/dawsonlpx3/async_utils/ApiManagerTask.java
+++
@@ -1,9 +0,0 @@
-package com.dawsonlpx3.async_utils;
-
-/**
- * Created by laborlyrene on 2017-12-01.
- */
-
-public class ApiManagerTask {
-
-}
---
+++ app/src/main/java/com/dawsonlpx3/async_utils/WhosFreeAsyncTask.java
@@ -0,0 +1,15 @@
+package com.dawsonlpx3.async_utils;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by laborlyrene on 2017-12-01.
+ */
+
+public class WhosFreeAsyncTask extends AsyncTask <Void, Void, Void>{
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+}

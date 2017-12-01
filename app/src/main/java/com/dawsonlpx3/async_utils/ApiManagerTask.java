1c822fea532cc9796880e93fae40b189b56e70e5
Lyrene <laborlyrene@gmail.com>
2017-12-01 16:00:34 -0500

    Added folders for the different fragments that will handle the api calls and their skeleton classes along with the ApiManagerTask util class which will handle the async call to the backend

---
+++ app/src/main/java/com/dawsonlpx3/async_utils/ApiManagerTask.java
@@ -0,0 +1,9 @@
+package com.dawsonlpx3.async_utils;
+
+/**
+ * Created by laborlyrene on 2017-12-01.
+ */
+
+public class ApiManagerTask {
+
+}
---
+++ app/src/main/java/com/dawsonlpx3/find_friends_feature/FindFriendFragment.java
@@ -0,0 +1,7 @@
+package com.dawsonlpx3.find_friends_feature;
+
+
+
+public class FindFriendFragment {
+    
+}
